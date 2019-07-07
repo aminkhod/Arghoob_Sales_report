42026bb1df8bcd40b2bc1d46d9ff745a7369c0e7
Mohammad Amin <khodamoradi1992@gmail.com>
2019-07-07 12:18:49 +0430

    Update main5.py

--- Webpage/main5.py
+++ Webpage/main5.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template, json, request
-from flask.ext.mysql import MySQL
+from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
