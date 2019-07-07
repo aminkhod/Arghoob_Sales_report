270d1c6892c83264b18aedfcfb0580bd3fe167c4
Mohammad Amin <khodamoradi1992@gmail.com>
2019-07-07 11:35:36 +0430

    7 July

--- Webpage/main5.py
+++ Webpage/main5.py
@@ -4,6 +4,7 @@ from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+    # read the posted values from the UI
 
 	
 @app.route("/")
@@ -22,17 +23,28 @@ def showSignUp():
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
- 
-    # read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
- 
-    # validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+	mysql = MySQL()
+	 
+	# MySQL configurations
+	app.config['MYSQL_DATABASE_USER'] = 'root'
+	app.config['MYSQL_DATABASE_PASSWORD'] = ''
+	app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+	app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+	mysql.init_app(app)
+	_password = ''
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	_hashed_password = generate_password_hash(_password)
+	cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+
+	data = cursor.fetchall()
+	 
+	if len(data) is 0:
+		conn.commit()
+		return json.dumps({'message':'User created successfully !'})
+	else:
+		return json.dumps({'error':str(data[0])})
 	
 if __name__ == "__main__":
     app.run(debug=True)
--- Webpage/static/js/jquery-3.4.11.js
+++ Webpage/static/js/jquery-3.4.11.js
@@ -10593,6 +10593,21 @@ if ( !noGlobal ) {
 
 
 
-
+$(function() {
+    $('#btnSignUp').click(function() {
+ 
+        $.ajax({
+            url: '/signUp',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
 return jQuery;
 } );
