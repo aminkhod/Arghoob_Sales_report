38a6ccc8a4b8d1875e5cbad0b728e944de6eb9b4
Mohammad Amin <khodamoradi1992@gmail.com>
2019-07-07 08:35:39 +0430

    Update main5.py

--- Webpage/main5.py
+++ Webpage/main5.py
@@ -1,8 +1,11 @@
 
 from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+	
 @app.route("/")
 def template():
     return render_template("template.html")
@@ -21,15 +24,29 @@ def showSignUp():
 def signUp():
  
     # read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
- 
-    # validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
-		
+
+	mysql = MySQL()
+	 
+	# MySQL configurations
+	app.config['MYSQL_DATABASE_USER'] = 'jay'
+	app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+	app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+	app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+	mysql.init_app(app)
+
+	conn = mysql.connect()
+	cursor = conn.cursor()
+
+	_hashed_password = generate_password_hash(_password)
+	cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+
+	data = cursor.fetchall()
+	 
+	if len(data) is 0:
+		conn.commit()
+		return json.dumps({'message':'User created successfully !'})
+	else:
+		return json.dumps({'error':str(data[0])})
+	
 if __name__ == "__main__":
     app.run(debug=True)
