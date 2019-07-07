4d2be2a6940f32ad2c695eeb6f6e30f2dc6904e3
Mohammad Amin <khodamoradi1992@gmail.com>
2019-07-07 11:42:49 +0430

    7 July

--- Webpage/main5.py
+++ Webpage/main5.py
@@ -7,14 +7,14 @@ import pandas as pd
 app = Flask(__name__)
     # read the posted values from the UI
 
-	
+
 @app.route("/")
 def template():
     return render_template("template.html")
 @app.route('/home')
 def home():
     return render_template("home.html")
-    
+
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -24,8 +24,8 @@ def showSignUp():
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
+	pd.read_csv('')
 
 
-	
 if __name__ == "__main__":
     app.run(debug=True)
