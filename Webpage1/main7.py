7d70da4207ae4ef99c9e8919c66520731b1fcf57
Mohammad Amin <khodamoradi1992@gmail.com>
2019-07-30 12:25:07 +0430

    30 July

--- Webpage1/main7.py
+++ Webpage1/main7.py
@@ -60,24 +60,24 @@ df = pd.read_csv('allMonthes.csv')
 yV = []
 for i in range(5):
     yV.append(df.values[:,i + 8])
-numListOfBranch = ['401 Co','402 Co','404 Co','405 Co','412 Co','416 Co','417 Co','423 Co',
-                   '424 Co','425 Co','426 Co','429 Co','444 Co','490 Co',]
-listOfBranch = ['401 Co', '401.VMS cost', '401.V.S.P.', '401.AVG. WEEK',
-                '402 Co', '402.VMS cost', '402.V.S.P.', '402.AVG. WEEK',
-                '404 Co', '404.VMS cost', '404.V.S.P.', '404.AVG. WEEK',
-                '405 Co', '405.VMS cost', '405.V.S.P.', '405.AVG. WEEK',
-                '412 Co', '412.VMS cost', '412.V.S.P.', '412.AVG. WEEK',
-                '416 Co', '416.VMS cost', '416.V.S.P.', '416.AVG. WEEK',
-                '417 Co', '417.VMS cost', '417.V.S.P.', '417.AVG. WEEK',
-                '423 Co', '423.VMS cost', '423.V.S.P.', '423.AVG. WEEK',
-                '424 Co', '424.VMS cost', '424.V.S.P.', '424.AVG. WEEK',
-                '425 Co', '425.VMS cost', '425.V.S.P.', '425.AVG. WEEK',
-                '426 Co', '426.VMS cost', '426.V.S.P.', '426.AVG. WEEK',
-                '429 Co', '429.VMS cost', '429.V.S.P.', '429.AVG. WEEK',
-                '444 Co', '444.VMS cost', '444.V.S.P.', '444.AVG. WEEK',
-                '490 Co', '490.VMS cost', '490.V.S.P.', '490.AVG. WEEK']
+numListOfBranch = ['401 Co','402 Co','404 Co','405 Co','412 Co','416 Co',
+                   '417 Co','423 Co', '424 Co','425 Co','426 Co','429 Co','444 Co','490 Co']
+listOfBranch = ['401 Co', '401.Arq COST', '401.Cost Price', '401.V.S.P.', '401.AVG. WEEK',
+                '402 Co', '402.Arq COST', '402.Cost Price', '402.V.S.P.', '402.AVG. WEEK',
+                '404 Co', '404.Arq COST', '404.Cost Price', '404.V.S.P.', '404.AVG. WEEK',
+                '405 Co', '405.Arq COST', '405.Cost Price', '405.V.S.P.', '405.AVG. WEEK',
+                '412 Co', '412.Arq COST', '412.Cost Price', '412.V.S.P.', '412.AVG. WEEK',
+                '416 Co', '416.Arq COST', '416.Cost Price', '416.V.S.P.', '416.AVG. WEEK',
+                '417 Co', '417.Arq COST', '417.Cost Price', '417.V.S.P.', '417.AVG. WEEK',
+                '423 Co', '423.Arq COST', '423.Cost Price', '423.V.S.P.', '423.AVG. WEEK',
+                '424 Co', '424.Arq COST', '424.Cost Price', '424.V.S.P.', '424.AVG. WEEK',
+                '425 Co', '425.Arq COST', '425.Cost Price', '425.V.S.P.', '425.AVG. WEEK',
+                '426 Co', '426.Arq COST', '426.Cost Price', '426.V.S.P.', '426.AVG. WEEK',
+                '429 Co', '429.Arq COST', '429.Cost Price', '429.V.S.P.', '429.AVG. WEEK',
+                '444 Co', '444.Arq COST', '444.Cost Price', '444.V.S.P.', '444.AVG. WEEK',
+                '490 Co', '490.Arq COST', '490.Cost Price', '490.V.S.P.', '490.AVG. WEEK', 'Latest SOH']
 
-productDetail = ['Sku', 'UPC', 'Catalogue N', 'Title', 'Label' ,'Cost Price' ,'V.S.P.']
+productDetail = ['Sku', 'UPC', 'Catalogue N', 'Title', 'Label' ,'Arq COST', 'Cost Price' ,'V.S.P.']
 header = productDetail.copy()
 header.extend(listOfBranch.copy())
 
@@ -101,10 +101,10 @@ def showSignUp():
 def btnMonthlySale():
     df = pd.read_csv('../Monthes/01-2019.csv')
     branchTot1 = []
-    for col in numListOfBranch:
+    for col in listOfBranch:
         branchTot1.append(df[col].sum())
     trace5 = go.Table(
-        header=dict(values=numListOfBranch),
+        header=dict(values=listOfBranch),
         cells=dict(values=branchTot1))
 
     df = pd.read_csv('../Monthes/02-2019.csv')
@@ -140,10 +140,12 @@ def btnMonthlySale():
     data = Data([trace5, trace6, trace7, trace8, trace9])
     layout = Layout(
         title='2019 sale for each months',
+        width = 1000,
         updatemenus=list([
             dict(
                 x=-0.05,
                 y=1,
+
                 yanchor='top',
                 buttons=list([
                     dict(
@@ -175,7 +177,6 @@ def btnMonthlySale():
                 )
             ]),
         )
-    fig = Figure(data=data, layout=layout)
     fig = Figure(data=data, layout=layout)
     plot(fig, filename='Monthly_Sale.html')
     return render_template("Monthly_Sale.html")
