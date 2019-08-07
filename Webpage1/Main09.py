7cd344a8e6d0d7c669d20d150624e314acecea75
Mohammad Amin <khodamoradi1992@gmail.com>
2019-08-07 13:16:50 +0430

    7 August

--- Webpage1/Main09.py
+++ Webpage1/Main09.py
@@ -269,97 +269,76 @@ def profitTable():
     feature = 'Bar'
     bar = create_plot(feature)
     return render_template('index.html', plot=bar)
-# Response(
-#         csv,
-#         mimetype="csv",
-#         headers={"Content-disposition":
-#                  "attachment; filename=Profit Table.csv"}),
 
-# @app.route('/index')
-# def index():
-#     feature = 'Bar'
-#     bar = create_plot(feature)
-#     return render_template('index.html', plot=bar)
 def create_plot(feature):
-    if feature == 'Bar':
-        monthesLenght = [31,28,31,30,31,30,31,31,30,31,30,31]
-        df = pd.read_csv('allMonthes.csv')
-        foo = [filesNoAdd[i].replace('.csv',' total sale of Goods') for i in range(len(filesNoAdd))]
-        list = ['Sku', 'UPC', 'Catalogue N', 'Label', 'Arq COST',
-                'Cost Price', 'V.S.P.', 'Latest SOH']
-        newdf = df[list]
+    monthesLenght = [31,28,31,30,31,30,31,31,30,31,30,31]
+    df = pd.read_csv('allMonthes.csv')
+    foo = [filesNoAdd[i].replace('.csv',' total sale of Goods') for i in range(len(filesNoAdd))]
+    list = ['Sku', 'UPC', 'Catalogue N', 'Label', 'Arq COST',
+            'Cost Price', 'V.S.P.', 'Latest SOH']
+    newdf = df[list]
 
-        totalQtySold = np.zeros(len(df['Sku']))
-        totalArghoobCost = np.zeros(len(df['Sku']))
-        totalArqhoobPrice = np.zeros(len(df['Sku']))
+    totalQtySold = np.zeros(len(df['Sku']))
+    totalArghoobCost = np.zeros(len(df['Sku']))
+    totalArqhoobPrice = np.zeros(len(df['Sku']))
 
-        for i in range(len(foo)):
-            cName = foo[i].replace(' total sale of Goods',': QTY SOLD')
-            newdf[cName] = df[foo[i]]
-            newdf[cName+'Arqoob Cost'] = df[foo[i]] * df['Arq COST']
-            newdf[cName+'QTY SOLD VALUE'] = df[foo[i]] * df['Cost Price']
-            list.extend([cName,cName+'Arqoob Cost',cName+'QTY SOLD VALUE'])
-            totalQtySold += newdf[cName]
-            totalArghoobCost += newdf[cName+'Arqoob Cost']
-            totalArqhoobPrice += newdf[cName+'QTY SOLD VALUE']
+    for i in range(len(foo)):
+        cName = foo[i].replace(' total sale of Goods',': QTY SOLD')
+        newdf[cName] = df[foo[i]]
+        newdf[cName+'Arqoob Cost'] = df[foo[i]] * df['Arq COST']
+        newdf[cName+'QTY SOLD VALUE'] = df[foo[i]] * df['Cost Price']
+        list.extend([cName,cName+'Arqoob Cost',cName+'QTY SOLD VALUE'])
+        totalQtySold += newdf[cName]
+        totalArghoobCost += newdf[cName+'Arqoob Cost']
+        totalArqhoobPrice += newdf[cName+'QTY SOLD VALUE']
 
-        qtyAvgDay = round(totalQtySold/np.sum(monthesLenght[:len(foo)]),2)
-        qtyAvgMonth = round(totalQtySold/len(foo),2)
-        valueAvgMonth = round(totalArqhoobPrice/len(foo),2)
-        qtyStockValue = newdf['Latest SOH'] * newdf['Cost Price']
-        DaysStockInHand = round(newdf['Latest SOH'] / qtyAvgDay,2)
-        WeeksStockInHand = round(DaysStockInHand/7,0)
-        MonthesStockInHand = round(DaysStockInHand/30,0)
-        i = 0
-        CurrentStockCoverUpto = []
-        for i in range(len(DaysStockInHand)):
-            try:
-                int(DaysStockInHand[i])
+    qtyAvgDay = round(totalQtySold/np.sum(monthesLenght[:len(foo)]),2)
+    qtyAvgMonth = round(totalQtySold/len(foo),2)
+    valueAvgMonth = round(totalArqhoobPrice/len(foo),2)
+    qtyStockValue = newdf['Latest SOH'] * newdf['Cost Price']
+    DaysStockInHand = round(newdf['Latest SOH'] / qtyAvgDay,2)
+    WeeksStockInHand = round(DaysStockInHand/7,0)
+    MonthesStockInHand = round(DaysStockInHand/30,0)
+    i = 0
+    CurrentStockCoverUpto = []
+    for i in range(len(DaysStockInHand)):
+        try:
+            int(DaysStockInHand[i])
 
-            except:
-                DaysStockInHand[i] = 0
-        for N in DaysStockInHand:
-            if(N!=0):
-                CurrentStockCoverUpto.append(str(datetime.now() + timedelta(days=N))[:10])
-            else:
-                CurrentStockCoverUpto.append('null')
-            i +=1
+        except:
+            DaysStockInHand[i] = 0
+    for N in DaysStockInHand:
+        if(N!=0):
+            CurrentStockCoverUpto.append(str(datetime.now() + timedelta(days=N))[:10])
+        else:
+            CurrentStockCoverUpto.append('null')
+        i +=1
 
 
-        newdf['Total Qty Sold'], newdf['Total Arghoob Cost'],newdf['Total Arqhoob Price'],\
-        newdf['Qty Avg Day'], newdf['Qty Avg Month'], newdf['Value Avg Month'],\
-        newdf['Qty Stock Value'], newdf['Days Stock in Hand'],newdf['Current stock cover upto (Week)'], \
-        newdf['Current stock cover upto (Month)'], newdf['Current stock cover upto (Date)'] = \
-                    totalQtySold, totalArghoobCost,totalArqhoobPrice,qtyAvgDay, qtyAvgMonth,\
-                    valueAvgMonth, qtyStockValue,DaysStockInHand, WeeksStockInHand, MonthesStockInHand,\
-                    CurrentStockCoverUpto
-        newdf.to_csv('Profit Table.csv')
-        with open('Profit Table.csv') as fp:
-            csv = fp.read()
-        list.extend(['Total Qty Sold', 'Total Arghoob Cost', 'Total Arqhoob Price',
-                     'Qty Avg Day', 'Qty Avg Month','Value Avg Month', 'Qty Stock Value',
-                     'Days Stock in Hand', 'Current stock cover upto (Week)',
-                     'Current stock cover upto (Month)','Current stock cover upto (Date)'])
-        trace = go.Table(
-            header=dict(values=list),
-            cells=dict(values=np.transpose(newdf.values[:,:])))
-        layout = Layout(
-            title='Profit Table',
-            width=3500
-            )
-        data = [trace]
-        fig = Figure(data=data, layout=layout)
-    else:
-        N = 1000
-        random_x = np.random.randn(N)
-        random_y = np.random.randn(N)
+    newdf['Total Qty Sold'], newdf['Total Arghoob Cost'],newdf['Total Arqhoob Price'],\
+    newdf['Qty Avg Day'], newdf['Qty Avg Month'], newdf['Value Avg Month'],\
+    newdf['Qty Stock Value'], newdf['Days Stock in Hand'],newdf['Current stock cover upto (Week)'], \
+    newdf['Current stock cover upto (Month)'], newdf['Current stock cover upto (Date)'] = \
+                totalQtySold, totalArghoobCost,totalArqhoobPrice,qtyAvgDay, qtyAvgMonth,\
+                valueAvgMonth, qtyStockValue,DaysStockInHand, WeeksStockInHand, MonthesStockInHand,\
+                CurrentStockCoverUpto
+    newdf.to_csv('Profit Table.csv')
+    with open('Profit Table.csv') as fp:
+        csv = fp.read()
+    list.extend(['Total Qty Sold', 'Total Arghoob Cost', 'Total Arqhoob Price',
+                 'Qty Avg Day', 'Qty Avg Month','Value Avg Month', 'Qty Stock Value',
+                 'Days Stock in Hand', 'Current stock cover upto (Week)',
+                 'Current stock cover upto (Month)','Current stock cover upto (Date)'])
+    trace = go.Table(
+        header=dict(values=list),
+        cells=dict(values=np.transpose(newdf.values[:,:])))
+    layout = Layout(
+        title='Profit Table',
+        width=3500
+        )
+    data = [trace]
+    fig = Figure(data=data, layout=layout)
 
-        # Create a trace
-        data = [go.Scatter(
-            x = random_x,
-            y = random_y,
-            mode = 'markers'
-        )]
 
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -374,22 +353,5 @@ def change_features():
 
     return graphJSON
 
-# @app.route('/getPlotCSV')
-# def getPlotCSV():
-# 	try:
-# 		return render_template('downloads.html')
-# 	except Exception as e:
-# 		return str(e)
-# @app.route("/getPlotCSV")
-# def getPlotCSV():
-#     with open('allMonthes.csv') as fp:
-#         csv = fp.read()
-#
-#
-    # return Response(
-    #     csv,
-    #     mimetype="csv",
-    #     headers={"Content-disposition":
-    #              "attachment; filename=myplot.csv"})
 if __name__ == "__main__":
     app.run(debug=True)
