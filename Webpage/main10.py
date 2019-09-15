20f78d6425934aef53213c59297e91fb96eaf432
Mohammad Amin <khodamoradi1992@gmail.com>
2019-09-15 14:32:47 +0430

    15 September

--- Webpage/main10.py
+++ Webpage/main10.py
@@ -322,7 +322,8 @@ def create_plot():
         cells=dict(values=np.transpose(newdf.values[:,:])))
     layout = Layout(
         title='Profit Table',
-        width=3600,
+        width=3800,
+		height=620
         )
     data = [trace]
     fig = Figure(data=data, layout=layout)
